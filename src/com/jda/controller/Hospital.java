ec27b72954f608b2a0828ae43699f3b5d9398820
Dhiraj <Dhiraj.Kanake@jda.com>
2018-08-06 12:14:29 +0000

    clinique update

--- src/com/jda/controller/Hospital.java
+++ src/com/jda/controller/Hospital.java
@@ -1,5 +1,67 @@
 package com.jda.controller;
 
-public class Hospital {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.jda.model.Doctor;
+import com.jda.service.Methods;
+import com.jda.serviceImpl.Application;
+import com.jda.utility.Utility;
+
+public class Hospital {
+	static JSONParser parser = new JSONParser();
+
+	public static void main(String args[]) throws FileNotFoundException, IOException, ParseException {
+		Doctor doc = new Doctor();
+		Utility utility = new Utility();
+		Methods mt =new Methods();
+		boolean flag = false, demo = false;
+		int doctorNumber = 0;
+		HashMap<Integer, Doctor[]> doctorList = new HashMap<>();
+		while (true) {
+			System.out.println("***Clinique Management***");
+			System.out.println("1.Enter New Doctor");
+			System.out.println("2.Search Doctor");
+			System.out.println("3.search Patient");
+			System.out.println("4.Take appointment");
+			System.out.println("5. print report");
+			System.out.println("6.exit");
+			int choice = utility.inputInteger();
+			switch (choice) {
+			case 1: {
+				doctorNumber += 1;
+				doctorList.put(doctorNumber, mt.createDoctorList());
+				utility.storeIntoFile(doctorList);
+				break;
+			}
+			case 2: {
+				utility.searchDoctor();
+				break;
+			}
+			case 3: {
+				
+				break;
+			}
+			case 4: {
+				utility.editPersonDetail(bookList);
+				break;
+			}
+			case 5: {
+				utility.deletePerson(bookList);
+				break;
+			}
+			case 6: {
+				flag = true;
+				break;
+			}
+			}
+			if (flag)
+				break;
+		}
+
+	}
 }
--- src/com/jda/utility/Utility.java
+++ src/com/jda/utility/Utility.java
@@ -5,68 +5,87 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import com.jda.model.Doctor;
 
 public class Utility {
 	static JSONParser parser = new JSONParser();
 	Scanner scanner;
-	public Utility()
-	{
-		scanner =new Scanner(System.in);
+
+	public Utility() {
+		scanner = new Scanner(System.in);
 	}
-	public String inputString()
-	{
+
+	public String inputString() {
 		return scanner.next();
 	}
-	public Long inputLong()
-	{
+
+	public Long inputLong() {
 		return scanner.nextLong();
 	}
-	public int inputInteger()
-	{
+
+	public int inputInteger() {
 		return scanner.nextInt();
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public void storeIntoFile(HashMap<Integer,Doctor[]> list) throws FileNotFoundException, IOException, ParseException
+	public void storeIntoFile(HashMap<Integer, Doctor[]> list)throws FileNotFoundException, IOException, ParseException 
+	{
+		String filename = "/home/bridgelabz/Documents/doctors.json";
+		Object obj = parser.parse(new FileReader(filename));
+		JSONObject object = (JSONObject) obj;
+		JSONObject addressBook = (JSONObject) object.get("Doctors");
+		JSONArray jArray = (JSONArray) addressBook.get("List");
+		Doctor doc[] = list.get(1);
+		HashMap<String, JSONObject> hMap2 = new HashMap<>(); // creating 2nd
+		                                                  
+		int lengthOfBook = doc.length;
+		for (int i = 0; i < lengthOfBook; i++) {
+			JSONObject jObject = doc[i].createObject();
+			jArray.add(jObject);
+		}
+		addressBook.put("List", jArray);
+		hMap2.put("Doctors", addressBook);
+
+		JSONObject newObjectdemo = new JSONObject(hMap2); // converting that
+		                                                  // hashmap2 into
+		                                                  // jasonobject
+
+		@SuppressWarnings("resource")
+		FileWriter file = new FileWriter(filename); // writing into file
+
+		file.write(newObjectdemo.toJSONString());
+		// System.out.println("passed"+companyName);
+		file.flush();
+
+	}
+	public void searchDoctor() throws FileNotFoundException, IOException, ParseException
 	{
 		String filename = "/home/bridgelabz/Documents/doctors.json";
 		Object obj = parser.parse(new FileReader(filename));
 		JSONObject object = (JSONObject)obj;
 		JSONObject addressBook=(JSONObject) object.get("Doctors");
 		JSONArray jArray=(JSONArray)addressBook.get("List");
-		Doctor doc[]=list.get(1);
-		HashMap<String,JSONObject> hMap2=new HashMap<>();      //creating 2nd hashmap to store all the entries from file
-		
-		int lengthOfBook=doc.length;
-		for(int i=0;i<lengthOfBook;i++)
+		System.out.println("Enter the name of the Doctor to search");
+		String Name=inputString();
+	 @SuppressWarnings("rawtypes")
+	Iterator itr = jArray.iterator();
+		while(itr.hasNext())
 		{
-			JSONObject jObject = doc[i].createObject();
-			jArray.add(jObject);
+			JSONObject jobject=(JSONObject) itr.next();
+			String name=(String) jobject.get("Name");
+			if(Name.equals(name))
+			{
+				System.out.println("Found"+jobject);
+				
+			}
 		}
-		addressBook.put("List", jArray);
-		hMap2.put("Doctors",addressBook );
-		
-		 
-		 JSONObject newObjectdemo=new JSONObject(hMap2);     //converting that hashmap2 into jasonobject
-		 
-		//System.out.println("jason"+newObjectdemo);
-		
-		@SuppressWarnings("resource")
-		FileWriter file = new FileWriter(filename);     //writing into file
-		
-		 file.write(newObjectdemo.toJSONString());
-		 //System.out.println("passed"+companyName);
-		 file.flush();
-		 
-		
 	}
 
 }
